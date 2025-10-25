05e86714a324bd05a36bf989643bde2e8c359da0
r03iuL <robiulislam1806@gmail.com>
2025-10-25 23:56:26 +0600

    feat:add services section

--- src/components/TrustedBy.jsx
+++ src/components/TrustedBy.jsx
@@ -4,7 +4,7 @@ import {company_logos} from "../assets/assets";
 const TrustedBy = () => {
   return (
     <div className="flex flex-col items-center px-4 sm:px-12 lg:px-24 xl:px-40 gap-10 text-gray-700 dark:text-white/80">
-      <h3 className="font-semibold"> Trusted by Leading Companies</h3>
+      <h3 className="font-semibold text-2xl"> Trusted by Leading Companies</h3>
       <div className="flex items-center justify-center flex-wrap gap-10 m-4">
         {company_logos.map((logo,index)=>(
             <img key={index} src={logo} alt="" className="max-h-5 sm:max-h-6 dark:drop-shadow-xl" />
